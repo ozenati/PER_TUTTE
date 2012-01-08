7c750a96051a635554bd139e0fb55eebcdde9f26
ozenati <ziriug@gmail.com>
2012-01-08 18:32:30 +0100

    Ajout de la version de tutte parallele asynchrone (pas enncore fonctionnelle)

---
+++ include/separator.h
@@ -0,0 +1,11 @@
+#ifndef SEPARATOR_H
+#define SEPARATOR_H
+
+vector<vector<MyNode *> *> *separateMap2Vectors(map<int, MyNode *> *all_nodes);
+
+map<int, MyNode *> *convertGraph2Map(Graph *graph);
+
+void applyVectors2Graph(vector<vector<MyNode *> *> *vectors, Graph * graph);
+
+
+#endif	/* SEPARATOR_H */
---
+++ include/tutte.h
@@ -0,0 +1,6 @@
+#ifndef TUTTE_H
+#define TUTTE_H
+
+void tutte_parallel_asynchrone(vector<vector<MyNode *> *> *vectors, double eps);
+
+#endif
